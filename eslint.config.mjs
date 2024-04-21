775ffa85d5dc8621170ad6c8ccdedd00c85ba442
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2024-04-21 14:15:54 +0200

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+// eslint.config.mjs
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  vue: {
+    overrides: {
+      'vue/max-attributes-per-line': 'error',
+    },
+  },
+  ignores: ['src/vite-env.d.ts'],
+})
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+/**
+ * main.ts
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Plugins
+import { createApp } from 'vue'
+import App from './App.vue'
+import { registerPlugins } from '@/plugins'
+
+// Components
+
+// Composables
+
+const app = createApp(App)
+
+registerPlugins(app)
+
+app.mount('#app')
