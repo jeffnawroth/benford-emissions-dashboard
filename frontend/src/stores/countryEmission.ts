8ca39a54b6fd3a6140ed738741c79b0f29455f3d
Jeff Nawroth <idol.analysiert_0n@icloud.com>
2024-06-02 19:18:54 +0200

    build: fix

--- frontend/src/stores/countryEmission.ts
+++ frontend/src/stores/countryEmission.ts
@@ -64,7 +64,7 @@ export const useCountryEmissionStore = defineStore('countryEmission', () => {
 
   // Methods
 
-  const URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8000'
+  const URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:8080'
 
   // Fetch emissions data from the API
   async function fetchCO2EmissionsData() {
